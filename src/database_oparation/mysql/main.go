997503c0b24c552dce4a5ab784a4aeb16395fb72
Ren Yoshida <ren.yoshida1992@gmail.com>
2022-02-12 16:19:52 +0900

    サンプルの追加

--- src/database_oparation/mysql/main.go
+++ src/database_oparation/mysql/main.go
@@ -1,72 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"GoSamples/src/database_oparation/base/common"
 	_ "github.com/go-sql-driver/mysql" //直接的な記述が無いが、インポートしたいものに対しては"_"を頭につける決まり
 	"github.com/jinzhu/gorm"
-	"io/ioutil"
 )
 
 func main() {
+	// 接続文字列
+	// {ユーザ名} + ":" + {パスワード} + "@tcp(" + {ホスト名} + ":" + {ポート} + ")/" + {データベース} + "?parseTime=true"
+	connect := "test:test@tcp(localhost:3306)/test?parseTime=true"
 	// DBに接続
-	db := gormConnect()
-	// ModelとDB名を一致させて取得できるようにする
-	db.SingularTable(true)
-	// メソッド終了時にDBをクローズする
-	defer db.Close()
-	// 結果格納用の変数
-	var records []User
-	// 全件取得
-	db.Find(&records)
-	fmt.Println(records)
-}
-
-// dbsetting.jsonから接続設定を読み込む
-func readConfig() DbConfig {
-	raw, err := ioutil.ReadFile("dbConfig.json")
-	if err != nil {
-		panic(err)
-	}
-
-	var conf DbConfig
-	json.Unmarshal(raw, &conf)
-	return conf
-}
-
-/**
- * DBに接続
- * DBの各種設定はdbConfig.jsonを適宜変更する
- */
-func gormConnect() *gorm.DB {
-	conf := readConfig()
-	DBMS := "mysql"
-	USER := conf.User
-	PASS := conf.Password
-	DBNAME := conf.DbName
-	HOSTNAME := conf.HostName
-	PORT := conf.Port
-	// MySQLだと文字コードの問題で"?parseTime=true"を末尾につける必要がある
-	CONNECT := USER + ":" + PASS + "@tcp(" + HOSTNAME + ":" + PORT + ")/" + DBNAME + "?parseTime=true"
-	db, err := gorm.Open(DBMS, CONNECT)
-
+	db, err := gorm.Open("mysql", connect)
 	if err != nil {
 		panic(err.Error())
 	}
-	return db
-}
+	// 最後にDBをクローズ
+	defer db.Close()
 
-// User テーブルのmodel
-type User struct {
-	UserName string
-	Password string
-}
-
-// DbConfig dbConfig.jsonのmodel
-type DbConfig struct {
-	User     string
-	Password string
-	DbName   string
-	HostName string
-	Port     string
+	// DB操作部分については以下の関数内を参照
+	common.Operation(db)
 }
--- src/database_oparation/sql_lite/main.go
+++ src/database_oparation/sql_lite/main.go
@@ -1,22 +1,20 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"GoSamples/src/database_oparation/base/common"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
 	// DBに接続
-	db := dbConnect()
-
-	// 
-
-	// 最後にDBをクローズ
-	defer db.Close()
-
-}
-
-func dbConnect() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		panic("failed to connect database")
 	}
-	return db
+	// 最後にDBをクローズ
+	defer db.Close()
+
+	// DB操作部分については以下の関数内を参照
+	common.Operation(db)
 }
